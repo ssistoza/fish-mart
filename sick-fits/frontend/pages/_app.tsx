ee7d87ad1ae584461660d506b7b3542af8d37206
Shane Avery Sistoza <me@ssistoza.dev>
2021-03-12 15:11:30 -0500

    Create a progress bar

--- sick-fits/frontend/pages/_app.tsx
+++ sick-fits/frontend/pages/_app.tsx
@@ -1,4 +1,11 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+import '../components/styles/nprogress.css';
 
 export default function App({ Component, pageProps }) {
   return (

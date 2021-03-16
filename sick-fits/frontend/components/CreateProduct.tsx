31285cd3a0c082608360e31e7b2570ff5777afc4
Shane Avery Sistoza <me@ssistoza.dev>
2021-03-16 13:59:30 -0400

    Route to product on add

--- sick-fits/frontend/components/CreateProduct.tsx
+++ sick-fits/frontend/components/CreateProduct.tsx
@@ -1,7 +1,9 @@
 import { gql, useMutation } from '@apollo/client';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
+import { ALL_PRODUCTS_QUERY } from './Products';
 
 const CREATE_PRODUCT_MUTATION = gql`
   mutation CREATE_PRODUCT_MUTATION(
@@ -38,6 +40,7 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
 
@@ -47,6 +50,9 @@ export default function CreateProduct() {
         evt.preventDefault();
         const resp = await createProduct();
         clearForm();
+        Router.push({
+          pathname: `/product/${resp.data.createProduct.id}`,
+        });
       }}
     >
       <DisplayError error={error} />

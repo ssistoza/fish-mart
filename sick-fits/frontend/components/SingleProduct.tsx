2feb149610a9bcff45f6bb4277d9483ddff4cdf5
Shane Avery Sistoza <me@ssistoza.dev>
2021-03-16 14:28:10 -0400

    Build SingleProductPage

---
+++ sick-fits/frontend/components/SingleProduct.tsx
@@ -0,0 +1,63 @@
+import { gql, useQuery } from '@apollo/client';
+import Head from 'next/head';
+import styled from 'styled-components';
+import DisplayError from './ErrorMessage';
+
+const SingleProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  justify-content: center;
+  align-items: top;
+  gap: 2rem;
+  img {
+    width: 100%;
+    object-fit: contain;
+  }
+`;
+
+const PRODUCT_QUERY = gql`
+  query PRODUCT_QUERY($id: ID!) {
+    Product(where: { id: $id }) {
+      id
+      name
+      price
+      description
+      photo {
+        altText
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+export default function SingleProduct({ id }) {
+  const { data, loading, error } = useQuery(PRODUCT_QUERY, {
+    variables: {
+      id,
+    },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <DisplayError error={error} />;
+  const { Product } = data;
+
+  return (
+    <SingleProductStyles>
+      <Head>
+        <title>Sick Fits | {Product.name}</title>
+      </Head>
+      <img
+        src={Product.photo.image.publicUrlTransformed}
+        alt={Product.photo.altText}
+      />
+      <div className="details">
+        <h2>{Product.name}</h2>
+        <p>{Product.description}</p>
+      </div>
+    </SingleProductStyles>
+  );
+}

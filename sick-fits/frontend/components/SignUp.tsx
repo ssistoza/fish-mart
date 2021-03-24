2da63f7ad493484e268329d2b52dfda4e1eeada4
Shane Avery Sistoza <me@ssistoza.dev>
2021-03-24 14:26:48 -0400

    Create signup page

---
+++ sick-fits/frontend/components/SignUp.tsx
@@ -0,0 +1,82 @@
+import { gql, useMutation } from '@apollo/client';
+import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
+import Form from './styles/Form';
+import { CURRENT_USER_QUERY } from './User';
+
+const SIGNUP_MUTATAION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { name: $name, email: $email, password: $password })
+  }
+`;
+
+export default function SignUp() {
+  const { inputs, handleChange, resetForm } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATAION, {
+    variables: inputs,
+  });
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    const res = await signup().catch(console.error);
+    resetForm();
+  };
+
+  return (
+    <Form method="POST" onSubmit={handleSubmit}>
+      <h2>Sign Up For an Account</h2>
+      <DisplayError error={error} />
+      <fieldset>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please go ahead and sign
+            in!
+          </p>
+        )}
+
+        <label htmlFor="name">
+          Your name
+          <input
+            type="name"
+            name="name"
+            placeholder="Your name"
+            autoComplete="name"
+            value={inputs.name}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            autoComplete="email"
+            value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="password">
+          Password
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            autoComplete="password"
+            value={inputs.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Sign In</button>
+      </fieldset>
+    </Form>
+  );
+}

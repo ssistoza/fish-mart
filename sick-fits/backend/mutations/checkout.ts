cb62dacec09c336589d1508e06abb6bb899e8c36
Shane Avery Sistoza <me@ssistoza.dev>
2021-05-13 14:03:26 -0400

    complete order

--- sick-fits/backend/mutations/checkout.ts
+++ sick-fits/backend/mutations/checkout.ts
@@ -69,5 +69,33 @@ export default async function checkout(
 
   console.log(charge);
   // 4. Convert the cartItems to OrderItems.
+  const orderItems = cartItems.map((cartItem) => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id } },
+    };
+
+    return orderItem;
+  });
+
   // 5. Create the order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId } },
+    },
+  });
+
+  // 6. Clean up old cart item
+  const cartItemIds = cartItems.map((cartItem) => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds,
+  });
+
+  return order;
 }

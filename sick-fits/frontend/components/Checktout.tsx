8857068c015d7e0ddb292b65dd2b9e6905536cd4
Shane Avery Sistoza <me@ssistoza.dev>
2021-05-13 13:53:42 -0400

    stripe with successful payments

--- sick-fits/frontend/components/Checktout.tsx
+++ sick-fits/frontend/components/Checktout.tsx
@@ -9,6 +9,7 @@ import { useState } from 'react';
 import nProgress from 'nprogress';
 import styled from 'styled-components';
 import SickButton from './styles/SickButton';
+import { gql, useMutation } from '@apollo/client';
 
 const CheckoutFormStyles = styled.form`
   box-shadow: 0 1px 2px 2px rgba(0, 0, 0, 0.04);
@@ -19,11 +20,28 @@ const CheckoutFormStyles = styled.form`
   grid-gap: 1rem;
 `;
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation CREATE_ORDER_MUTATION($token: String!) {
+    checkout(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        name
+      }
+    }
+  }
+`;
+
 const stripeLib = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY);
 
 export function ChecktoutForm() {
   const [error, setError] = useState<StripeError>();
   const [loading, setLoading] = useState(false);
+  const [checkout, { error: graphQLError }] = useMutation(
+    CREATE_ORDER_MUTATION
+  );
   const stripe = useStripe();
   const elements = useElements();
 
@@ -42,8 +60,17 @@ export function ChecktoutForm() {
     // 4. Handle any errors from stripe
     if (error) {
       setError(error);
+      nProgress.done();
+      return;
     }
     // 5. Send the token from step to our keystone server, through a custom mutation.
+    const order = await checkout({
+      variables: {
+        token: paymentMethod.id,
+      },
+    });
+    console.log(`Finished with the order!!`);
+    console.log(order);
     // 6. Change the page to view the order.
     // 7. Close the cart
     // 8. Turn loader off.
@@ -54,6 +81,7 @@ export function ChecktoutForm() {
   return (
     <CheckoutFormStyles onSubmit={handleSubmit}>
       {error && <p style={{ fontSize: 12 }}>error.message</p>}
+      {graphQLError && <p style={{ fontSize: 12 }}>graphQLError.message</p>}
       <CardElement />
       <SickButton>Checkout Now</SickButton>
     </CheckoutFormStyles>

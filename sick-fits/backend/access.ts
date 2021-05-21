e249a0d3437feb5e6e26f7e714452154198111ee
Shane Avery Sistoza <me@ssistoza.dev>
2021-05-21 15:41:06 -0400

    permissions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,7 +1,22 @@
 // At it's simplest, the access contorl returns a boolean depending on the users session
 
-import { ListAccessArgs } from './types';
+import { permissionsList } from './schemas/fields';
+import { ListAccessArgs, Permission } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs): boolean {
   return !!session;
 }
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+export const permissions = {
+  ...generatedPermissions,
+};
